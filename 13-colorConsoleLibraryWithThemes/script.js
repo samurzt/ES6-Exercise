968f022aab78682cd377db5b41a5354c9cc2b0ca
samurzt <samu.joseph@razorthink.net>
2015-12-08 10:45:40 +0530

    Added color console library with themes

--- 13-colorConsoleLibraryWithThemes/script.js
+++ 13-colorConsoleLibraryWithThemes/script.js
@@ -1,13 +1,12 @@
 "use strict";
-import * as styleConsoleLib from './styleConsoleLib.js';
+import styleConsoleLib from './styleConsoleLib.js';
 
 let themesAvailable = styleConsoleLib.getAvailableThemes();
 
 for (let i=0; i < themesAvailable.length; i++) {
     console.log("Trying theme", themesAvailable[i]);
-    styleConsoleLib.setConsoleTheme(themesAvailable[i]);
-    
-    styleConsoleLib.consoleStyleFunctions.log("hi log");
+    styleConsoleLib.setConsoleTheme(themesAvailable[i]);   
+    styleConsoleLib.log("hi log");
     styleConsoleLib.consoleStyleFunctions.info("hi info");
     styleConsoleLib.consoleStyleFunctions.warn("hi warn");
     styleConsoleLib.consoleStyleFunctions.error("hi error");
--- 13-colorConsoleLibraryWithThemes/styleConsoleLib.js
+++ 13-colorConsoleLibraryWithThemes/styleConsoleLib.js
@@ -1,57 +1,4 @@
 "use strict";
-let consoleStyleFunctions = {
-    error: (message) => {
-        console.error("%c" + message, "background: red; color: white");
-    },
-    info: (message) => {
-        console.info("%c" + message, "background: blue; color: white");
-    },
-    log: (message) => {
-        console.log("%c" + message, "background: black; color: white");
-    },
-    warn: (message) => {
-        console.warn("%c" + message, "background: yellow; color: black");
-    },
-};
-
-
-function setStyleToConsole(consoleStyle) {
-    switch (consoleStyle.func) {
-        case 'log':
-            consoleStyleFunctions.log =
-                function(message) {
-                    console.log("%c" + message, consoleStyle.style);
-                };
-            break;
-
-        case 'info':
-            consoleStyleFunctions.info =
-                function(message) {
-                    console.info("%c" + message, consoleStyle.style);
-                };
-            break;
-
-        case 'warn':
-            consoleStyleFunctions.warn =
-                function(message) {
-                    console.warn("%c" + message, consoleStyle.style);
-                };
-            break;
-
-        case 'error':
-            consoleStyleFunctions.error =
-                function(message) {
-                    console.error("%c" + message, consoleStyle.style);
-                };
-            break;
-
-        default:
-            break;
-    }
-
-}
-
-
 
 let colorConsoleThemes = [{
         themeName: "theme1",
@@ -75,6 +22,25 @@ let colorConsoleThemes = [{
 
 ];
 
+let currentTheme = colorConsoleThemes[0];
+
+
+let consoleStyleFunctions = {
+    log: (message) => {
+        console.log("%c" + message, colorConsoleThemes[currentTheme].log);
+    },
+    info: (message) => {
+        console.info("%c" + message, colorConsoleThemes[currentTheme].info);
+    },
+
+    warn: (message) => {
+        console.warn("%c" + message, colorConsoleThemes[currentTheme].warn);
+    },
+    error: (message) => {
+        console.error("%c" + message, colorConsoleThemes[currentTheme].error);
+    },
+};
+
 function getAvailableThemes() {
     return colorConsoleThemes.map((x) => {
         return x.themeName;
@@ -82,32 +48,19 @@ function getAvailableThemes() {
 }
 
 function setConsoleTheme(themeName) {
-    for (let i = 0; i < colorConsoleThemes.length; i++) {
-        if (themeName == colorConsoleThemes[i].themeName) {
-            setStyleToConsole({
-                func: 'log',
-                style: colorConsoleThemes[i].log
-            });
-            setStyleToConsole({
-                func: 'info',
-                style: colorConsoleThemes[i].info
-            });
-            setStyleToConsole({
-                func: 'warn',
-                style: colorConsoleThemes[i].warn
-            });
-            setStyleToConsole({
-                func: 'error',
-                style: colorConsoleThemes[i].error
-            });
-            break;
-        }
+    let themeIndex = getAvailableThemes().indexOf(themeName);
+    if (themeIndex != -1) {
+        currentTheme = themeIndex;
     }
 }
 
 module.exports = {
-    setStyleToConsole: setStyleToConsole,
     setConsoleTheme: setConsoleTheme,
     getAvailableThemes: getAvailableThemes,
-    consoleStyleFunctions: consoleStyleFunctions
+    consoleStyleFunctions: consoleStyleFunctions,
+    log: consoleStyleFunctions.log,
+    info: consoleStyleFunctions.info,
+    warn: consoleStyleFunctions.warn,
+    error: consoleStyleFunctions.error,
+
 };
