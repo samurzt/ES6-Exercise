3fa6144bd36c98a9f5be8aab6ec1fa6f45a28d8d
samurzt <samu.joseph@razorthink.net>
2015-12-08 12:18:31 +0530

    added es5 code for currying

--- 14-curryingRectangularPrism/script.js
+++ 14-curryingRectangularPrism/script.js
@@ -1,3 +1,20 @@
+//ES5 code
+function shape(l)
+{ 
+    return function(b)
+    { 
+        return function(h)
+        {
+            return l*b*h;
+        } 
+    }
+}
+oneD=shape(1);
+twoD=oneD(2);
+threeD=twoD(3);
+console.log(threeD);
 
-shape=(l)=>(b)=>(h)=>{ return l*b*h; }
-console.log(shape(1)(2)(3));
+
+//ES6 Code
+shapeRec=(l)=>(b)=>(h)=>{ return l*b*h; }
+console.log(shapeRec(1)(2)(3));
